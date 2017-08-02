4fefc26b6a1775b98b03854e187d81b3f2c05df4
Rishi <rishi.yemme@gmail.com>
2017-08-01 21:23:23 -0400

    added api ai function for response

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.post("/webhook", function (req, res) {
             entry.messaging.forEach(function(event) {
                 if (event.postback) {
                     processPostback(event);
+                }else if(event.message && event.message.text){
+                    aiResponse();
                 }
             });
         });
@@ -70,7 +72,7 @@ function processPostback(event) {
                 name = bodyObj.first_name;
                 greeting = "Hi " + name + ". ";
             }
-            var message = greeting + "My name is Spotibot. I can tell you a lot about musical artists. Who do you waant to know about?";
+            var message = greeting + "My name is Spotibot. I can tell you a lot about musical artists. Who do you want to know about?";
             sendMessage(senderId, {text: message});
         });
     }
@@ -127,24 +129,27 @@ function sendMessage(recipientId, message) {
 
 }
 
-app.post('/ai', (req, res)=>{
-    if(req.body.result.action === 'artist'){
-        //call music artist api
-        request({
-            url: "https://graph.facebook.com/v2.6/me/messages",
-            qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-            method: "POST",
-            json: {
-                recipient: {id: recipientId},
-                message: {message : "ACTION RECEIVED"}
-            }
-        }, function(error, response, body) {
-            if (error) {
-                console.log("Error sending message: " + response.error);
-            }
-        });
-    }
-});
+function aiResponse() {
+    app.post('/ai', (req, res)=>{
+        if(req.body.result.action === 'artist'){
+            //call music artist api
+            request({
+                url: "https://graph.facebook.com/v2.6/me/messages",
+                qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+                method: "POST",
+                json: {
+                    recipient: {id: recipientId},
+                    message: {message : "ACTION RECEIVED"}
+                }
+            }, function(error, response, body) {
+                if (error) {
+                    console.log("Error sending message: " + response.error);
+                }
+            });
+        }
+    });
+}
+
 
 
 // catch 404 and forward to error handler
