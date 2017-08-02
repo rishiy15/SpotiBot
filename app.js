e7e8cafe95682cc946e21fd91a0ed59bed09f6da
Rishi <rishi.yemme@gmail.com>
2017-08-01 21:06:51 -0400

    fixed callback bug

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
+let isFirstTime = true;
+
 // Server index page
 app.get("/", function (req, res) {
     res.send("Deployed!");
@@ -68,7 +70,7 @@ function processPostback(event) {
                 name = bodyObj.first_name;
                 greeting = "Hi " + name + ". ";
             }
-            var message = greeting + "My name is Spotibot. I can tell you a lot about musical artists. Who do you want to know about?";
+            var message = greeting + "My name is Spotibot. I can tell you a lot about musical artists. Who do you waant to know about?";
             sendMessage(senderId, {text: message});
         });
     }
@@ -76,53 +78,73 @@ function processPostback(event) {
 
 // sends message to user using api.ai's api
 function sendMessage(recipientId, message) {
-    let apiai = apiaiApp.textRequest(message, {
-        sessionId: 'talk_spotify'
-    });
 
-    apiai.on('response', (response)=>{
-        let aiText = response.fulfillment.speech;
+    if(isFirstTime){
+        request({
+            url: "https://graph.facebook.com/v2.6/me/messages",
+            qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+           method: "POST",
+            json: {
+              recipient: {id: recipientId},
+              message: message,
+                }
+            }, function(error, response, body) {
+            if (error) {
+                console.log("Error sending message: " + response.error);
+            }
+        });
+        isFirstTime = false;
+    }else{
+        let apiai = apiaiApp.textRequest(message, {
+            sessionId: 'talk_spotify'
+        });
 
+        apiai.on('response', (response)=>{
+            let aiText = response.fulfillment.speech;
+
+            request({
+                url: "https://graph.facebook.com/v2.6/me/messages",
+                qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+                method: "POST",
+                json: {
+                    recipient: {id: recipientId},
+                    message: {message : aiText}
+                }
+            }, function(error, response, body) {
+                if (error) {
+                    console.log("Error sending message: " + response.error);
+                }
+            });
+        });
+
+        apiai.on('error',(error)=> {
+            console.log(error);
+        });
+
+        apiai.end();
+    }
+
+
+}
+
+app.post('/ai', (req, res)=>{
+    if(req.body.result.action === 'artist'){
+        //call music artist api
         request({
             url: "https://graph.facebook.com/v2.6/me/messages",
             qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
             method: "POST",
             json: {
                 recipient: {id: recipientId},
-                message: {message : aiText}
+                message: {message : "ACTION RECEIVED"}
             }
         }, function(error, response, body) {
             if (error) {
                 console.log("Error sending message: " + response.error);
             }
         });
-    });
-
-    apiai.on('error',(error)=> {
-        console.log(error);
-    });
-
-    apiai.end();
-}
-
-// app.post('/ai', (req, res)=>{
-//     if(req.body.result.action === 'artist'){
-//         //call music artist api
-//         request({
-//             url: "https://graph.facebook.com/v2.6/me/messages",
-//             qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-//             method: "POST",
-//             json: {
-//                 recipient: {id: recipientId},
-//                 message: {message : "ACTION RECEIVED"}
-//             }
-//         }, function(error, response, body) {
-//             if (error) {
-//                 console.log("Error sending message: " + response.error);
-//             }
-//         });
-//     }
-// });
+    }
+});
 
 
 // catch 404 and forward to error handler
