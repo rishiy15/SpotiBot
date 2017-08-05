2a1ceb6a0fc1ed65faeeb947dcc3d272b9577133
Rishi <rishi.yemme@gmail.com>
2017-08-05 17:29:16 -0400

    testing method used to call external api

--- app.js
+++ app.js
@@ -134,24 +134,24 @@ function sendMessage(event) {
 
 }
 
-    // app.post('/ai', (req, res)=>{
-    //     if(req.body.result.action === 'artist'){
-    //         //call music artist api
-    //         request({
-    //             url: "https://graph.facebook.com/v2.6/me/messages",
-    //             qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    //             method: "POST",
-    //             json: {
-    //                 recipient: {id: recipientId},
-    //                 message: {message : "ACTION RECEIVED"}
-    //             }
-    //         }, function(error, response, body) {
-    //             if (error) {
-    //                 console.log("Error sending message: " + response.error);
-    //             }
-    //         });
-    //     }
-    // });
+    app.post('/ai', (req, res)=>{
+        if(req.body.result.action === 'artist'){
+            //call music artist api
+            request({
+                url: "https://graph.facebook.com/v2.6/me/messages",
+                qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+                method: "POST",
+                json: {
+                    recipient: {id: recipientId},
+                    message: {message : "ACTION RECEIVED"}
+                }
+            }, function(error, response, body) {
+                if (error) {
+                    console.log("Error sending message: " + response.error);
+                }
+            });
+        }
+    });
 
 
 
